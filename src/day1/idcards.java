7c19d908d3becd806284ed53915bccb99d48b2ed
kishan kn <kishanpuc1471@gmail.com>
2026-08-17 15:26:03 +0530

    inrial commit

---
+++ src/day1/idcards.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class idcards {
+    public static void main(String[]args)
+    {
+        String id1="AdR";
+        String id2="PAN";
+
+        if(id1 == "DL")
+        {
+            System.out.println("you are distinction pass");
+        }
+        else if(id2 == "LC")
+        {
+            System.out.println("your firstclass");
+        }
+        else{
+            System.out.println("Sorry not allowed neither don't have "+id1+ " or" +id2);
+        }
+    }
+}
