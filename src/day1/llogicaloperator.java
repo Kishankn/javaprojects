aeaa298a5909ae3bb7ace769eb53b6e0dcc1235f
kishan kn <kishanpuc1471@gmail.com>
2026-08-17 14:56:18 +0530

    inrial commit

---
+++ src/day1/llogicaloperator.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class llogicaloperator {
+    public static void main(String[]args){
+        int age=23;
+
+        boolean student=true;
+        System.out.println(age>=20 && student);
+        System.out.println(age !=11 && !student);
+        System.out.println(age>=20 && student);
+    }
+}
