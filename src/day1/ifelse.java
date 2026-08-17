fd058af8c58a3dc22602d07e876e9cfeeae421ef
kishan kn <kishanpuc1471@gmail.com>
2026-08-17 15:13:04 +0530

    inrial commit

---
+++ src/day1/ifelse.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class ifelse {
+    public static void main(String[]args)
+    {
+        int marks=75;
+        if(marks > 60)
+        {
+            System.out.println("You are distintion pass");
+        }
+        else
+        {
+            System.out.println("Your firstclass");
+        }
+    }
+}
+
