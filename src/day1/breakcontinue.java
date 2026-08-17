d2e8e33c48fbda44742e1d1e6800e2ffbeb7d2d2
kishan kn <kishanpuc1471@gmail.com>
2026-08-17 15:59:41 +0530

    inrial commit

---
+++ src/day1/breakcontinue.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class breakcontinue {
+    public static void main(String[]args){
+        for (int i=0;i<=10;i++){
+            if(i==5){
+                continue;
+            }
+            if(i==8){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
