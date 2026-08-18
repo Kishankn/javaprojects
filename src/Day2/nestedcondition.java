58897e9f448b2f5cebf1803945cd5d3e16b06854
kishan kn <kishanpuc1471@gmail.com>
2026-08-18 10:54:51 +0530

    basics

---
+++ src/Day2/nestedcondition.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class nestedcondition {
+    public static void main(String[] args){
+        int marks=85;
+        if(marks >= 35) {
+            if (marks <= 95)
+            {
+                System.out.println("secured distinction");
+            }
+        }
+    }
+}
