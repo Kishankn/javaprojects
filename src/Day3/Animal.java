cd3464a52552e4d69a986aa914e266f424958233
kishan kn <kishanpuc1471@gmail.com>
2026-08-19 12:59:50 +0530

    basics

---
+++ src/Day3/Animal.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class Animal {
+    void eat(){
+        System.out.println("dog eats");
+    }
+    static class dog extends Animal{
+        void bark(){
+            System.out.println("dog barks");
+        }
+    }
+        public static void main(String[] args) {
+            dog myDog = new dog();
+            myDog.eat();
+            myDog.bark();
+    }}
+
