9797d128511439b7d4988c7b8c9915c311c0b268
kishan kn <kishanpuc1471@gmail.com>
2026-08-20 10:23:01 +0530

    basics

---
+++ src/Day4/TryCatchBasics.java
@@ -0,0 +1,17 @@
+package Day4;
+import java.util.Scanner;
+public class TryCatchBasics {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        try {
+            System.out.println("Enter numerator :");
+            int a = sc.nextInt();
+            System.out.println("Enter denominator :");
+            int b = sc.nextInt();int result=a/b;
+            System.out.println("the result of calculation is "+result);
+        }catch (ArithmeticException e){
+            System.out.println("error:cannotdivide by zero");
+        }
+    }
+
+}
