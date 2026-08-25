23cc0802849c3570eab1cf75497db46dfbc5c9e5
kishan kn <kishanpuc1471@gmail.com>
2026-08-25 16:36:07 +0530

    basics

---
+++ src/Day6/TwoArray.java
@@ -0,0 +1,12 @@
+package Day6;
+
+public class TwoArray {
+    public static void main(String[] args){
+        int[][] arr= {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+        System.out.println(arr[1][2]);
+    }
+}
