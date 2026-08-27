62281ded721dd35c5e1aac239fc33909a5b23880
kishan kn <kishanpuc1471@gmail.com>
2026-08-27 10:24:06 +0530

    basics

---
+++ src/Day7/strtray.java
@@ -0,0 +1,11 @@
+package Day7;
+
+public class strtray {
+    public static void main(String[] args){
+        String str="saptagiri";
+        for(int i=0;i<str.length();i++)
+        {
+            System.out.println("Char of STR index is = " + str.charAt(i));
+        }
+    }
+}
