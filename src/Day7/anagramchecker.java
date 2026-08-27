562522956636c1195fbd7e7853708455a5cc53ac
kishan kn <kishanpuc1471@gmail.com>
2026-08-27 15:24:51 +0530

    basics

---
+++ src/Day7/anagramchecker.java
@@ -0,0 +1,26 @@
+package Day7;
+import java.util.Arrays;
+public class anagramchecker {
+        public static boolean isAnagramSort(String str1, String str2) {
+            str1 = str1.replaceAll("\\s", "").toLowerCase();
+            str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+            if (str1.length() != str2.length()) {
+                return false;
+            }
+
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            return Arrays.equals(charArray1, charArray2);
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isAnagramSort("Listen", "Silent"));
+            System.out.println(isAnagramSort("Hello", "World"));
+        }
+    }
+
