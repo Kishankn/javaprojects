43f513c292577a8f5ca770129751ace07cee259f
kishan kn <kishanpuc1471@gmail.com>
2026-08-27 09:24:47 +0530

    basics

---
+++ src/Day7/string.java
@@ -0,0 +1,11 @@
+package Day7;
+
+public class string {
+    public static void main(String[] args){
+        String str="hello";
+        System.out.println("my string"+str);
+        System.out.println("string length=="+str.length());
+        System.out.println("the first character"+str.charAt(0));
+        System.out.println("the second character"+str.charAt(1));
+    }
+}
