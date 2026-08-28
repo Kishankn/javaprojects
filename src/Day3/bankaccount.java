874315aaf1d71fe72f440ebe493c929ddcb71625
kishan kn <kishanpuc1471@gmail.com>
2026-08-28 09:53:12 +0530

    Java basics

--- src/Day3/bankaccount.java
+++
@@ -1,9 +0,0 @@
-package Day3;
-
-public class bankaccount {
-    public static void main(String[] args){
-        bankaccount b = new bankaccount();
-        System.out.println("account object"+b);
-    }
-
-}
