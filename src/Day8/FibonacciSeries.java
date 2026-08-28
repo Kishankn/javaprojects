a6530a968a53981a40f77bc12d76b61f0d2cab70
kishan kn <kishanpuc1471@gmail.com>
2026-08-28 10:14:01 +0530

    java basics

---
+++ src/Day8/FibonacciSeries.java
@@ -0,0 +1,14 @@
+package Day8;
+
+public class FibonacciSeries {
+    static int fibonacci(int n){
+        if (n<=1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println("Fibonacci = " + fibonacci(n));
+    }
+}
