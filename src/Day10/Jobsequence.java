794c9b8f71d7f5e8f547d36bdbd2f4d00a3b8593
kishan kn <kishanpuc1471@gmail.com>
2026-09-01 15:25:19 +0530

    Java basics

--- src/Day10/Jobsequence.java
+++ src/Day10/Jobsequence.java
@@ -15,7 +15,32 @@ public class Jobsequence {
         job[] jobs = {
                 new job("a", 2, 100),
                 new job("b", 1, 50),
+                new job("C",2,200),
+                new job("D",1,80)
         };
+        Arrays.sort(jobs,
+                (a,b)->b.profit-a.profit);
+        int maxDeadline=0;
+        for(job job:jobs){
+            maxDeadline = Math.max(maxDeadline,job.deadline);
+        }
+        String[]slots= new String[maxDeadline + 1];
+        int totalProfit=0;
+        for(job job:jobs){
+            for(int slot = job.deadline; slot>=1;slot--){
+                if(slots[slot]==null){
+                    slots[slot]=job.name;
+                    totalProfit+=job.profit;
+                    break;
+                }
+            }
+        }
+        System.out.println("Schedule:");
+        for(int i=1;i<slots.length;i++){
+            System.out.println(
+                    "Slot"+i+"->"+slots[i]
+            );
+        }
+        System.out.println("Maximum Profit = "+totalProfit);
     }
-
-    }
+}
